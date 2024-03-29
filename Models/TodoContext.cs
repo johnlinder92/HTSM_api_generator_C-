31e6b17b71081f758f00fa15ea8c3fd5c6cf4bc4
johna <john.artur.linder@gmail.com>
2024-03-29 21:50:59 +0100

    Add project files.

---
+++ Models/TodoContext.cs
@@ -0,0 +1,12 @@
+﻿namespace HTSM_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class TodoContext : DbContext
+{
+    public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TodoItem> TodoItems { get; set; } = null!;
+}
